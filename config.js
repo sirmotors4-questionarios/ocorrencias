00fa02fabbc3c04470160db1911e775919bba73c
sirmotors4-questionarios <sirmotors4@gmail.com>
2026-09-21 09:21:13 +0200

    Add form configuration

---
+++ config.js
@@ -0,0 +1,7 @@
+window.OCORRENCIAS_CONFIG = {
+  // URL do Power Automate que receberá as submissões.
+  submissionUrl: "",
+
+  // Coloque false depois de inserir o URL real do Power Automate.
+  demoMode: true
+};
